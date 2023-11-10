3b7702cc9703a8656d129157f3c867fa42b87420
Karatkevich Aliaksei <torlesh18112003@gmail.com>
2023-11-11 00:47:53 +0300

    feat: add fromJson

--- src/main/java/ru/clevertec/Main.java
+++ src/main/java/ru/clevertec/Main.java
@@ -1,7 +1,126 @@
 package ru.clevertec;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.google.gson.Gson;
+import ru.clevertec.model.ChampionsLeague;
+import ru.clevertec.model.Player;
+import ru.clevertec.model.Team;
+import ru.clevertec.util.JsonConverter;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        Gson gson = new Gson();
+        Player playerA1 = new Player();
+        playerA1.setId(UUID.randomUUID());
+        playerA1.setName(null);
+        playerA1.setSurname("Surname A1");
+        playerA1.setDateBirth(LocalDate.of(1990, 1, 1));
+        playerA1.setNumber(7);
+
+        Player playerA2 = new Player();
+        playerA2.setId(UUID.randomUUID());
+        playerA2.setName("Player A2");
+        playerA2.setSurname("Surname A2");
+        playerA2.setDateBirth(LocalDate.of(1992, 5, 15));
+        playerA2.setNumber(10);
+
+        Player playerA3 = new Player();
+        playerA3.setId(UUID.randomUUID());
+        playerA3.setName("Player A3");
+        playerA3.setSurname("Surname A3");
+        playerA3.setDateBirth(LocalDate.of(1988, 11, 30));
+        playerA3.setNumber(5);
+
+        Player playerA4 = new Player();
+        playerA4.setId(UUID.randomUUID());
+        playerA4.setName("Player A4");
+        playerA4.setSurname("Surname A4");
+        playerA4.setDateBirth(LocalDate.of(1993, 7, 20));
+        playerA4.setNumber(9);
+
+        Team teamA1 = new Team();
+        teamA1.setId(UUID.randomUUID());
+        teamA1.setName("Team A1");
+        teamA1.setCountry("Country A");
+        teamA1.setPlayers(Arrays.asList(playerA1, playerA2));
+
+        Team teamA2 = new Team();
+        teamA2.setId(UUID.randomUUID());
+        teamA2.setName("Team A2");
+        teamA2.setCountry("Country A");
+        teamA2.setPlayers(Arrays.asList(playerA3, playerA4));
+
+        Player playerB1 = new Player();
+        playerB1.setId(UUID.randomUUID());
+        playerB1.setName("Player B1");
+        playerB1.setSurname("Surname B1");
+        playerB1.setDateBirth(LocalDate.of(1991, 4, 10));
+        playerB1.setNumber(8);
+
+        Player playerB2 = new Player();
+        playerB2.setId(UUID.randomUUID());
+        playerB2.setName("Player B2");
+        playerB2.setSurname("Surname B2");
+        playerB2.setDateBirth(LocalDate.of(1989, 8, 25));
+        playerB2.setNumber(11);
+
+        Player playerB3 = new Player();
+        playerB3.setId(UUID.randomUUID());
+        playerB3.setName("Player B3");
+        playerB3.setSurname("Surname B3");
+        playerB3.setDateBirth(LocalDate.of(1994, 2, 28));
+        playerB3.setNumber(6);
+
+        Player playerB4 = new Player();
+        playerB4.setId(UUID.randomUUID());
+        playerB4.setName("Player B4");
+        playerB4.setSurname("Surname B4");
+        playerB4.setDateBirth(LocalDate.of(1995, 9, 5));
+        playerB4.setNumber(12);
+
+        Team teamB1 = new Team();
+        teamB1.setId(UUID.randomUUID());
+        teamB1.setName("Team B1");
+        teamB1.setCountry("Country B");
+        teamB1.setPlayers(Arrays.asList(playerB1, playerB2));
+
+        Team teamB2 = new Team();
+        teamB2.setId(UUID.randomUUID());
+        teamB2.setName("Team B2");
+        teamB2.setCountry("Country B");
+        teamB2.setPlayers(Arrays.asList(playerB3, playerB4));
+
+        ChampionsLeague championsLeague = new ChampionsLeague();
+        championsLeague.setId(UUID.randomUUID());
+        championsLeague.setDateStart(OffsetDateTime.parse("2023-11-07T12:34:56Z"));
+        Map<Character, List<Team>> groups = new HashMap<>();
+        groups.put('A', Arrays.asList(teamA1, teamA2));
+        groups.put('B', Arrays.asList(teamB1, teamB2));
+        championsLeague.setGroups(groups);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        try {
+            String str = mapper.writeValueAsString(playerA1);
+            Player player = mapper.readValue(str, Player.class);
+            System.out.println(player);
+            Player player1 = (Player) JsonConverter.fromJson(str, Player.class);
+            System.out.println(player1);
+            //TODO не работет с null
+//            String str = mapper.writeValueAsString(teamA1);
+//            Team player = mapper.readValue(str, Team.class);
+//            System.out.println(player);
+//            Team player1 = (Team) JsonConverter.fromJson(str, Team.class);
+//            System.out.println(player1);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
--- src/main/java/ru/clevertec/util/JsonConverter.java
+++ src/main/java/ru/clevertec/util/JsonConverter.java
@@ -5,15 +5,18 @@ import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Collection;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class JsonConverter {
 
     private final static DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
 
     public static String toJson(Object obj) {
+        if (obj == null) {
+            throw new NullPointerException();
+        }
         StringBuilder jsonBuilder = new StringBuilder();
         jsonBuilder.append("{");
 
@@ -99,5 +102,82 @@ public class JsonConverter {
         }
 
         return toJson(obj);
+    }
+
+    //{"id":"8cd1a26f-2fb0-48db-9da9-3d55cd962538","dateStart":"2023-11-07T12:34:56.000+0000","groups":{"A":[{"id":"ab649309-170a-4061-8b9a-79a7d94721c7","name":"Team A1","country":"Country A","players":[{"id":"ca0abcb0-c6e9-40cd-88b9-10b00975653c","name":"Player A1","surname":"Surname A1","dateBirth":"1990-01-01","number":7},{"id":"ea7902cc-b84c-4a88-816f-e13e790bbc57","name":"Player A2","surname":"Surname A2","dateBirth":"1992-05-15","number":10}]},{"id":"baf89272-073f-49c4-9059-64a196532986","name":"Team A2","country":"Country A","players":[{"id":"4a28f7f8-d915-4a9a-b834-e274b5658345","name":"Player A3","surname":"Surname A3","dateBirth":"1988-11-30","number":5},{"id":"d82498de-359b-4524-9420-6ec5426b17b7","name":"Player A4","surname":"Surname A4","dateBirth":"1993-07-20","number":9}]}],"B":[{"id":"1ac11052-e13a-4a54-92c0-1f9c933bc2f8","name":"Team B1","country":"Country B","players":[{"id":"8e3fd4ee-2d7a-4744-98a2-61835ad26b74","name":"Player B1","surname":"Surname B1","dateBirth":"1991-04-10","number":8},{"id":"66b0d93a-0b08-4ddf-b0a6-3b43c784b3f7","name":"Player B2","surname":"Surname B2","dateBirth":"1989-08-25","number":11}]},{"id":"f6f5cf8c-b64c-48e8-8fdf-fdedc7816ae6","name":"Team B2","country":"Country B","players":[{"id":"d7050647-3157-44cb-ab04-92b68290f217","name":"Player B3","surname":"Surname B3","dateBirth":"1994-02-28","number":6},{"id":"139f3bac-9924-4814-a80e-bd633e9316c2","name":"Player B4","surname":"Surname B4","dateBirth":"1995-09-05","number":12}]}]}}
+
+
+    public static Object fromJson(String json, Class<?> clazz) {
+        try {
+            Object obj = clazz.getDeclaredConstructor().newInstance();
+            Map<String, Object> map = parseToMap(json);
+
+            Field[] fields = clazz.getDeclaredFields();
+            for (Field field : fields) {
+                field.setAccessible(true);
+                String fieldName = field.getName();
+                if (map.containsKey(fieldName)) {
+                    Object value = map.get(fieldName);
+                    setFieldValue(obj, field, value);
+                }
+            }
+            // System.out.println(map);
+            return obj;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static Map<String, Object> parseToMap(String json) {
+        Map<String, Object> map = new HashMap<>();
+
+        Pattern pattern = Pattern.compile(
+                "(\"\\w+\" ?: ?\"?[\\w. \\-,]+\"?)"
+        );
+        //TODO вложенность
+        Matcher matcher = pattern.matcher(json);
+        while (matcher.find()) {
+            String[] keyVal = matcher.group().split(" ?: ?");
+            String key = keyVal[0].substring(1, keyVal[0].length()-1);
+            String value = keyVal[1];
+            if (value.startsWith("\"") && value.endsWith("\"")) {
+                value = value.substring(1, value.length() - 1);
+            }
+            map.put(key, value);
+        }
+        return map;
+    }
+
+    private static void setFieldValue(Object obj, Field field, Object value) throws IllegalAccessException {
+        if (value == null) {
+            field.set(obj, null);
+        } else {
+            Class<?> fieldType = field.getType();
+            switch (fieldType.getSimpleName()) {
+                case "String":
+                    field.set(obj, value.toString());
+                    break;
+                case "Integer":
+                case "int":
+                    field.set(obj, Integer.parseInt(value.toString()));
+                    break;
+                case "UUID":
+                    field.set(obj, UUID.fromString(value.toString()));
+                    break;
+                case "LocalDate":
+                    field.set(obj, LocalDate.parse(value.toString()));
+                    break;
+                case "OffsetDateTime":
+                    field.set(obj, OffsetDateTime.parse(value.toString()));
+                    break;
+                case "ZonedDateTime":
+                    field.set(obj, ZonedDateTime.parse(value.toString()));
+                    break;
+                    //TODO еще классы
+                default:
+                    throw new IllegalArgumentException("Unsupported field type: " + fieldType.getSimpleName());
+            }
+        }
     }
 }
